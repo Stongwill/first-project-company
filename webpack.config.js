57e18445abccde9927edcfdc925a60cce4a6d3f5
taviskaron09 <taviskaron09@gmail.com>
2022-07-15 16:19:18 +0300

    new pages style

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,41 @@
 "use strict";
-
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 let mode = "development";
 process.env.NODE_ENV === "production" && (mode = "production");
-
 module.exports = {
   context: path.resolve(__dirname, "src"),
   mode: mode,
   entry: {
-   main: "./index.js",
+    main: "./index.js",
   },
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js", 
+    path: path.resolve(__dirname, "dist"), 
     clean: true,
   },
-  devtool: "source-map",
+  devtool: "source-map", 
   optimization: {
     splitChunks: {
-      chunks: "all"
-    }
+      chunks: "all",
+    },
   },
   devServer: {
-    port: "3000"
+    port: "3000",
   },
   plugins: [
-    new HtmlWebpackPlugin({template: "./index.html"}),
-    new MiniCssExtractPlugin({filename: "[name].[contenthash].css"}),
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
   ],
   module: {
     rules: [
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
+      { 
+        test: /\.html$/i, 
+        loader: "html-loader" 
       },
       {
-        test: /\.(sa|sc|c).ss$/,
+        test: /\.(sa|sc|c)ss$/,
         use: [
           mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
           "css-loader",
@@ -45,7 +43,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(jpg|png|svg|jpeg|gif)$/i,
+        test: /\.(png|gif|svg|jpg|jpeg)$/i,
       },
       {
         test: /\.(ttf|woff|woff2|eot|otf)$/i,
