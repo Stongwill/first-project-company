f362747278002cf4d625bd7f5fc0fda8daa589bf
taviskaron09 <taviskaron09@gmail.com>
2022-07-19 14:52:07 +0300

    final project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './styles/style.scss';
 
+const button = document.querySelector('#bur');
+const popup = document.querySelector('#popup');
+const menu = document.querySelector('.footer__mobile').cloneNode(1);
+const body = document.body;
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    popup.classList.toggle('open');
+    body.classList.toggle('noscroll')
+    popup.appendChild(menu);
+
+})
